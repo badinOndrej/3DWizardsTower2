0b43fb51868b81e76382c79860b14732a53b5473
badinOndrej <badin.ondrej@outlook.cz>
2021-01-07 06:33:45 +0100

    rendering rewrite
    both walls & sprites now work

--- levelLoop.py
+++ levelLoop.py
@@ -6,14 +6,20 @@ from functools import cmp_to_key
 
 DISPLAY = None
 
+textureDebug = False
+
 playerPos = {"x":0, "y":0}
-playerAng = 0
+playerDir = {"x": -1, "y": 0}
+rotSpeed = 0.1
 playerSpeed = 0.075
 clock = pygame.time.Clock()
 
+planeX = 0
+planeY = 0.66
+
 drawMap = False
 
-w = 640
+w = 160
 h = 480
 
 fov = math.radians(60)
@@ -42,7 +48,7 @@ levelMapDims = [
     {"x":12, "y":12}
 ]
 levelPlayerData = [
-    {"x":9.5, "y":9.5, "a":math.pi}
+    {"x":9.5, "y":9.5, "aX":-1, "aY":0}
 ]
 levelTextures = [
     [
@@ -56,7 +62,7 @@ levelDoors = [
 ]
 levelSprites = [
     [
-        (4.5, 4.5, "furn", 0)
+        (4, 4, "furn", 0)
     ]
 ]
 spriteTextures = [
@@ -67,6 +73,9 @@ levelBackground = pygame.image.load("grf/levelBackground.png")
 
 currLevelTextures = []
 currLevelSprites = []
+
+debugColoursA = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff, 0xffffff]
+debugColoursB = [0x800000, 0x008000, 0x000080, 0x808000, 0x800080, 0x008080, 0x808080]
 
 def sprite_compare(s1, s2):
     s1dist = math.sqrt((s1[0] - playerPos["x"]) ** 2 + (s1[1] - playerPos["y"]) ** 2)
@@ -79,54 +88,47 @@ def sprite_compare(s1, s2):
         return 1
 
 def raycast():
-    global fov, fovInc, playerAng, currLevel, w, h, DISPLAY
-    rayAngle = playerAng - (fov / 2)
     zBuffer = []
-    dirY = math.sin(playerAng)
-    dirX = math.cos(playerAng)
-    planeX = 0
-    planeY = 0.66
+    dirX = playerDir["x"]
+    dirY = playerDir["y"]
     for x in range(0, w):
-        # ray pos & dir
-        cameraX = float(2 * x / float(w) - 1) # x coord in camera space
-        rayPosX = playerPos["x"]
-        rayPosY = playerPos["y"]
+        cameraX = 2 * x / float(w) - 1
         rayDirX = dirX + planeX * cameraX
         rayDirY = dirY + planeY * cameraX
-        # where on map are we
-        mapX = int(rayPosX)
-        mapY = int(rayPosY)
-        # ray length from curr pos to the next x or y side
-        sideDistX = 0.
-        sideDistY = 0.
-        # ray length from one x/y side to the next
-        deltaDistX = math.sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX))
-        if rayDirY == 0: 
-            rayDirY = 0.00001
-        deltaDistY = math.sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY))
-        perpWallDist = 0.
-        # step direction (+/- 1)
+        # where on map
+        mapX = int(playerPos["x"])
+        mapY = int(playerPos["y"])
+        # length of ray from curr pos to next xy side
+        sideDistX = 0.0
+        sideDistY = 0.0
+        # from one xy side to the next
+        if rayDirX == 0: rayDirX = 0.00001
+        deltaDistX = abs(1 / rayDirX)
+        if rayDirY == 0: rayDirY = 0.00001
+        deltaDistY = abs(1 / rayDirY)
+        perpWallDist = 0.0
+        # step direction
         stepX = 0
         stepY = 0
         # wall hit
-        hit = 0 # was there?
-        side = 0 # NS or EW?
-        # step & init side dist
+        hit = 0
+        side = 0
+        # step & init sideDist
         if rayDirX < 0:
             stepX = -1
-            sideDistX = (rayPosX - mapX) * deltaDistX
+            sideDistX = (playerPos["x"] - mapX) * deltaDistX
         else:
             stepX = 1
-            sideDistX = (mapX + 1.0 * rayPosX) * deltaDistX
+            sideDistX = (mapX + 1.0 - playerPos["x"]) * deltaDistX
         if rayDirY < 0:
             stepY = -1
-            sideDistY = (rayPosY - mapY) * deltaDistY
+            sideDistY = (playerPos["y"] - mapY) * deltaDistY
         else:
             stepY = 1
-            sideDistY = (mapY + 1.0 - rayPosY) * deltaDistY
+            sideDistY = (mapY + 1.0 - playerPos["y"]) * deltaDistY
         # DDA
         while hit == 0:
-            if sideDistX < sideDistY:
+            if(sideDistX < sideDistY):
                 sideDistX += deltaDistX
                 mapX += stepX
                 side = 0
@@ -134,119 +136,103 @@ def raycast():
                 sideDistY += deltaDistY
                 mapY += stepY
                 side = 1
-            # check hit
-            if(levelMaps[currLevel][mapY][mapX] > 0):
-                hit = 1
-        # distance projected on camera direction (fix fisheye)
+            # was a wall hit
+            if levelMaps[currLevel][mapY][mapX] > 0: hit = 1
+        # calc distance
         if side == 0:
-            perpWallDist = abs((mapX - rayPosX + (1 - stepX) / 2) / rayDirX)
+            perpWallDist = (mapX - playerPos["x"] + (1 - stepX) / 2) / rayDirX
         else:
-            perpWallDist = abs((mapY - rayPosY + (1 - stepY) / 2) / rayDirY)
-        # append to zBuffer
+            perpWallDist = (mapY - playerPos["y"] + (1 - stepY) / 2) / rayDirY
         zBuffer.append(perpWallDist)
-        # calculate strip height
-        if perpWallDist == 0:
-            perpWallDist = 0.000001
-        lineHeight = abs(int(h / perpWallDist))
-        # lo-hi coords
-        drawStart = -lineHeight / 2 + h / 2
-        drawEnd = lineHeight / 2 + h / 2
-        # texture
-        tex = currLevelTextures[levelMaps[currLevel][mapY][mapX] - 1]
-        texWidth = tex.get_width()
-        texHeight = tex.get_height()
-        # wallX - where the wall was hit
-        wallX = 0
-        if side == 1:
-            wallX = rayPosX + ((mapY - rayPosY + (1 - stepY) / 2) / rayDirY) * rayDirX
+        # calc line height
+        lineHeight = int(h / perpWallDist)
+        drawStartY = -lineHeight / 2 + h / 2
+        drawEndY = lineHeight / 2 + h / 2
+        # get texture
+        texture = currLevelTextures[levelMaps[currLevel][mapY][mapX] - 1]
+        # if debug, draw solid colours
+        if textureDebug == True:
+            img = pygame.Surface((4, lineHeight))
+            col = debugColoursA[levelMaps[currLevel][mapY][mapX] - 1]
+            if side==1: col = debugColoursB[levelMaps[currLevel][mapY][mapX] - 1]
+            img.fill(col)
+            DISPLAY.blit(img, (x*4, drawStartY))
         else:
-            wallX = rayPosY + ((mapX - rayPosX + (1 - stepX) / 2) / rayDirX) * rayDirY
-        wallX -= math.floor((wallX))
-        # x coord on tex
-        texX = int(wallX * float(texWidth))
-        if side == 0 and rayDirX > 0:
-            texX = texWidth - texX - 1
-        if side == 1 and rayDirY < 0:
-            texX = texWidth - texX - 1
-        # draw wall
-        if lineHeight > 10000:
-            lineHeight = 10000
-            drawStart = -10000 / 2 + h / 2
-        stripe = pygame.Surface((1, texHeight)) # get texture stripe
-        stripe.blit(tex,(0,0),(texX,0,texX+1,texHeight))
-        stripe = pygame.transform.scale(stripe, (1, lineHeight)) # scale to proper size
-        DISPLAY.blit(stripe, ((w - x), drawStart)) # blit to screen
-        """ # ray    rayN == x
-        ray = {"x": playerPos["x"], "y": playerPos["y"]}
-        rayCos = dirX / 64
-        raySin = dirY / 64
-        # wall checking
-        wall = 0
-        while wall == 0:
-            ray["x"] += rayCos
-            ray["y"] += raySin
-            wall = levelMaps[currLevel][math.floor(ray["y"])][math.floor(ray["x"])]
-        # calculate distance to wall
-        distance = math.sqrt(math.pow(playerPos["x"] - ray["x"], 2) + math.pow(playerPos["y"] - ray["y"], 2))
-        distance *= math.cos(rayAngle - playerAng) # fisheye effect fix
-        zBuffer.append(distance)
-        wallHeight = math.floor((h / 2) / distance)
-        # textured walls
-        texture = currLevelTextures[wall - 1]
-        # get column within texture
-        texturePosX = math.floor(texture.get_width() * (ray["x"] + ray["y"])) % int(texture.get_width())
-        # extract texture strip
-        stripe = pygame.Surface((1, texture.get_height()))
-        stripe.blit(texture,(0,0),(texturePosX,0,texturePosX+1,texture.get_height()))
-        # scale to proper height
-        stripe = pygame.transform.scale(stripe,(4,2*wallHeight))
-        # darken by distance - SLOW!!!
-        darken = pygame.Surface(stripe.get_size())
-        shade = 255 / 0.5 / distance
-        if shade > 255:
-            shade = 255
-        darken.fill(pygame.Color((shade,shade,shade)))
-        stripe.blit(darken,(0,0),special_flags=BLEND_RGB_MULT)
-        # blit to screen
-        DISPLAY.blit(stripe, (rayN*4, (h/2) - wallHeight))
-        # increase ray angle by increment
-        rayAngle += fovInc """
+            # calc texture hit
+            wallX = 0
+            if side == 0:
+                wallX = playerPos["y"] + perpWallDist * rayDirY
+            else:
+                wallX = playerPos["x"] + perpWallDist * rayDirX
+            wallX -= math.floor((wallX))
+            # calc x coord
+            texX = int(wallX * texture.get_width())
+            if side == 0 and rayDirX > 0:
+                texX = texture.get_width() - texX - 1
+            if side == 1 and rayDirY < 0:
+                texX = texture.get_width() - texX - 1
+            # get strip
+            strip = pygame.Surface((1, texture.get_height()))
+            strip.blit(texture, (0,0), (texX,0,texX+1,texture.get_height()))
+            # if side 1, darken
+            if side == 1:
+                dark = pygame.Surface(texture.get_size()).convert_alpha()
+                dark.fill((0,0,0,127))
+                strip.blit(dark, (0,0))
+            # scale strip
+            strip = pygame.transform.scale(strip, (4, lineHeight))
+            # blit to screen
+            DISPLAY.blit(strip, (x*4, drawStartY))
+    drawSprites(zBuffer)
+    
+# handles drawing sprites
+def drawSprites(zBuffer):
     # sort sprites
     levelSprites[currLevel].sort(key=cmp_to_key(sprite_compare))
     # draw sprites
     for spr in levelSprites[currLevel]:
-        # sprite transform
+        # get texture
+        sprTex = currLevelSprites[spr[3]]
+        # sprite pos
         spriteX = spr[0] - playerPos["x"]
         spriteY = spr[1] - playerPos["y"]
-        invDet = 1.0 / (planeX * dirY - planeY * dirX)
-        transformX = invDet * (dirY * spriteX - dirX * spriteY)
-        transformY = invDet * (-planeY * spriteX + planeX * spriteY)
-        spriteSurfaceX = int((w / 2) * (1 + transformX / transformY))
-        # sprite height
+        # inverse matrix transform
+        invDet = 1.0 / (planeX * playerDir["y"] - playerDir["x"] * planeY)
+        transformX = invDet * (playerDir["y"] * spriteX - playerDir["x"] * spriteY)
+        transformY = invDet * (-planeY * spriteX + planeX * spriteY)    # z-depth
+        # screen x coord
+        spriteScreenX = int((w/2) * (1 + transformX / transformY))
+        # height on screen
         spriteHeight = abs(int(h / transformY))
-        drawStartY = -spriteHeight / 2 + h / 2
-        drawEndY = spriteHeight / 2 + h / 2
-        # sprite width
-        spriteWidth = abs(int(h / transformY))
-        drawStartX = int(-spriteWidth / 2 + spriteSurfaceX)
-        drawEndX = int(spriteWidth / 2 + spriteSurfaceX)
-        # draw sprite stripe by stripe
-        if spriteHeight < 1000:
-            for stripe in range(drawStartX, drawEndX):
-                sprTex = currLevelSprites[spr[3]]
-                texX = math.floor(256 * (stripe - (-spriteWidth / 2 + spriteSurfaceX)) * sprTex.get_width() / spriteWidth) / 256
-                if(transformY > 0 and stripe > 0 and stripe < w and transformY < zBuffer[stripe]):
-                    sprStrip = pygame.Surface((1, sprTex.get_height()))
-                    sprStrip.blit(sprTex, (0,0), (texX,0,texX+1,sprTex.get_height()))
-                    sprStrip = pygame.transform.scale(sprStrip, (1, 2 * spriteHeight))
-                    sprStrip.set_colorkey(0xff00ff)
-                    DISPLAY.blit(sprStrip, ((w -stripe), (h/2) - spriteHeight))
+        drawStartY = int(-spriteHeight / 2 + h / 2)
+        # width on screen
+        spriteWidth = abs(int(w / transformY))
+        drawStartX = int(-spriteWidth / 2 + spriteScreenX)
+        if drawStartX < 0: drawStartX = 0
+        drawEndX = int(spriteWidth / 2 + spriteScreenX)
+        if drawEndX >= w: drawEndX = w-1
+        # draw by strips
+        for stripX in range(drawStartX, drawEndX):
+            # texture x coord
+            texX = math.floor(256 * (stripX - (-spriteWidth / 2 + spriteScreenX)) * sprTex.get_width() / spriteWidth) / 256
+            # draw if
+            # in front of camera
+            # on screen - left
+            # on screen - right
+            # not occluded
+            if transformY > 0 and stripX > 0 and stripX < w and transformY < zBuffer[stripX]:
+                strip = pygame.Surface((1, sprTex.get_height()))
+                strip.blit(sprTex, (0,0), (texX, 0, texX+1, sprTex.get_height()))
+                strip.set_colorkey(0xff00ff)
+                strip = pygame.transform.scale(strip, (4, spriteHeight))
+                DISPLAY.blit(strip, (stripX * 4, drawStartY))
+        
 
 def updateGameVars(currLevel):
-    global playerAng
     playerPos["x"] = levelPlayerData[currLevel]["x"]
     playerPos["y"] = levelPlayerData[currLevel]["y"]
-    playerAng = levelPlayerData[currLevel]["a"]
+    playerDir["x"] = levelPlayerData[currLevel]["aX"]
+    playerDir["y"] = levelPlayerData[currLevel]["aY"]
     for tex in levelTextures[currLevel]:
         currLevelTextures.append((pygame.image.load(tex)).convert())
     for spr in levelSprites[currLevel]:
@@ -261,7 +247,7 @@ def initLevelLoop(surface):
     levelLoop()
 
 def levelLoop():
-    global playerAng, currLevel, playerSpeed, clock, drawMap, DISPLAY
+    global currLevel, playerSpeed, clock, drawMap, DISPLAY, planeX, planeY
     
     # game loop
     while True:
@@ -271,18 +257,18 @@ def levelLoop():
 
         if drawMap:
             # draw map
-            mapDx = 640 / levelMapDims[currLevel]["x"]
-            mapDy = 480 / levelMapDims[currLevel]["y"]
+            mapDx = 640 / levelMapDims[currLevel]["y"]
+            mapDy = 480 / levelMapDims[currLevel]["x"]
             for mapY in range(0,levelMapDims[currLevel]["y"]):
                 for mapX in range(0,levelMapDims[currLevel]["x"]):
-                    if levelMaps[currLevel][mapY][mapX] > 0:
+                    if levelMaps[currLevel][mapX][mapY] > 0:
                         pygame.draw.rect(DISPLAY, 0x44aaff, Rect((mapX * mapDx) + 1, (mapY * mapDy) + 1, mapDx - 2, mapDy - 2))
 
             # draw player on map
-            playerScreenX = playerPos["x"] * mapDx
-            playerScreenY = playerPos["y"] * mapDy
+            playerScreenX = playerPos["y"] * mapDx
+            playerScreenY = playerPos["x"] * mapDy
             pygame.draw.rect(DISPLAY, 0xffaa44, Rect(playerScreenX-2,playerScreenY-2,5,5))
-            pygame.draw.line(DISPLAY,0xffaa44,(playerScreenX, playerScreenY), (playerScreenX + 30*math.cos(playerAng), playerScreenY + 30*math.sin(playerAng)), 3)
+            pygame.draw.line(DISPLAY,0xffaa44,(playerScreenX, playerScreenY), (playerScreenX + 30*playerDir["y"], playerScreenY + 30*playerDir["x"]), 3)
 
         pygame.display.update()
         # manage events - quit, keyboard input
@@ -301,8 +287,8 @@ def levelLoop():
                     drawMap = not drawMap
                 if event.__dict__["key"] == K_SPACE:
                     # try to open hidden door
-                    newPosX = playerPos["x"] + playerSpeed * math.cos(playerAng)
-                    newPosY = playerPos["y"] + playerSpeed * math.sin(playerAng)
+                    newPosX = playerPos["x"] + playerSpeed * playerDir["x"]
+                    newPosY = playerPos["y"] + playerSpeed * playerDir["y"]
                     for door in levelDoors[currLevel]:
                         if (door[0] == math.floor(newPosX)) and (door[1] == math.floor(newPosY)):
                             if door[2] == "hidden":
@@ -321,24 +307,30 @@ def levelLoop():
                 sys.exit()
         # execute movement
         if keypressed["up"]:
-            newPosX = playerPos["x"] + playerSpeed * math.cos(playerAng)
-            newPosY = playerPos["y"] + playerSpeed * math.sin(playerAng)
+            newPosX = playerPos["x"] + playerSpeed * playerDir["x"]
+            newPosY = playerPos["y"] + playerSpeed * playerDir["y"]
             if levelMaps[currLevel][math.floor(newPosY)][math.floor(newPosX)] == 0:
                 playerPos["x"] = newPosX
                 playerPos["y"] = newPosY
         if keypressed["down"]:
-            newPosX = playerPos["x"] - playerSpeed * math.cos(playerAng)
-            newPosY = playerPos["y"] - playerSpeed * math.sin(playerAng)
+            newPosX = playerPos["x"] - playerSpeed * playerDir["x"]
+            newPosY = playerPos["y"] - playerSpeed * playerDir["y"]
             if levelMaps[currLevel][math.floor(newPosY)][math.floor(newPosX)] == 0:
                 playerPos["x"] = newPosX
                 playerPos["y"] = newPosY
-        if keypressed["left"]:
-            playerAng -= 0.1
-            if playerAng < 0:
-                playerAng = 2*math.pi + playerAng
         if keypressed["right"]:
-            playerAng += 0.1
-            if playerAng > 2*math.pi:
-                playerAng = playerAng - 2*math.pi
+            oldDirX = playerDir["x"]
+            playerDir["x"] = playerDir["x"] * math.cos(-rotSpeed) - playerDir["y"] * math.sin(-rotSpeed)
+            playerDir["y"] = oldDirX * math.sin(-rotSpeed) + playerDir["y"] * math.cos(-rotSpeed)
+            oldPlaneX = planeX
+            planeX = planeX * math.cos(-rotSpeed) - planeY * math.sin(-rotSpeed)
+            planeY = oldPlaneX * math.sin(-rotSpeed) + planeY * math.cos(-rotSpeed)
+        if keypressed["left"]:
+            oldDirX = playerDir["x"]
+            playerDir["x"] = playerDir["x"] * math.cos(rotSpeed) - playerDir["y"] * math.sin(rotSpeed)
+            playerDir["y"] = oldDirX * math.sin(rotSpeed) + playerDir["y"] * math.cos(rotSpeed)
+            oldPlaneX = planeX
+            planeX = planeX * math.cos(rotSpeed) - planeY * math.sin(rotSpeed)
+            planeY = oldPlaneX * math.sin(rotSpeed) + planeY * math.cos(rotSpeed)
         # limit framerate
         clock.tick(20)
